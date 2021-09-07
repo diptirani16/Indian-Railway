c27b46e0699426e2dc7f5b61ee38cb88ad1637d0
Dipti Rani <gdipti642@gmail.com>
2021-09-07 17:26:06 +0530

    Initial Commit

---
+++ src/components/LoginPage.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import './LoginPage.css';
+
+export default class LoginPage extends Component {
+
+    constructor(props) {
+        super (props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+        this.handleUsername = this.handleUsername.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleUsername (e) {
+        this.setState ({
+            username: e.target.value
+        })
+    }
+    
+    handlePassword (e) {
+        this.setState ({
+            password: e.target.value
+        })
+    }
+
+    handleSubmit (event) {
+        event.preventDefault();
+
+        const { username, password } = this.state;
+
+        fetch('https://indian-railway.vercel.app/api/login' , {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Basic ' + btoa(`${username}:${password}`)
+            }
+        })
+        .then(res => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+    }
+
+    render() {
+        return (
+            <div className="formContainer">
+                <form onSubmit={this.handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Username</label>
+                        <input type="text" onChange={this.handleUsername} value = {this.state.username} className="form-control" id="exampleInputEmail1"/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" onChange={this.handlePassword} value = {this.state.password}  className="form-control" id="exampleInputPassword1" />
+                    </div>
+                
+                    <button type="submit" className="btn btn-primary">Login</button>
+                   
+                </form>
+            </div>
+
+        )
+    }
+}
---
+++ src/components/MainPage.js
@@ -0,0 +1,9 @@
+class MainPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
